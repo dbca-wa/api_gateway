2cac2481155081cc84833185fa8bc0369f844a11
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-01-09 16:43:39 +0800

    Update to python cron.

--- startup.sh
+++ startup.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-# Start the first process
-env > /etc/.cronenv
-sed -i 's/\"/\\"/g' /etc/.cronenv
 cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 if [ $ENABLE_CRON == "True" ];
-then
-echo "Starting Cron"
-service cron start &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start cron: $status"
-  exit $status
-fi
-
+    then
+    echo "Starting Python Cron"
+    python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "Failed to start cron: $status"
+        exit $status
+    fi
+else
+   echo "ENABLE_CRON environment variable not set to True, python cron is not starting."
 fi
 
 if [ $ENABLE_WEB == "True" ];
     then
-echo "Starting Gunicorn"
-# Start the second process
-#cd /app/apigw/
-gunicorn apigw.wsgi --bind :8080 --config /app/gunicorn.ini
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start gunicorn: $status"
-  exit $status
-fi
+    echo "Starting Gunicorn"
+    /app/venv/bin/gunicorn  apigw.wsgi --bind :8080 --config /app/gunicorn.ini.py
+    status=$?
+    if [ $status -ne 0 ]; then
+    echo "Failed to start gunicorn: $status"
+    exit $status
+    fi
 else
-   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   echo "ENABLE_WEB environment variable not set to True, web server is not starting."
    /bin/bash
 fi
