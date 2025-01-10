160075c8771a2eb43afea142b6e0b5f0f7e40c74
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-01-10 12:12:36 +0800

    Only read environment file if it exists.

--- manage.py
+++ manage.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import confy
+#!/usr/bin/env python3
 import os
 import sys
-confy.read_environment_file()
 
+import confy
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apigw.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+dot_env = os.path.join(os.getcwd(), ".env")
+if os.path.exists(dot_env):
+    confy.read_environment_file()
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boranga.settings")
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
