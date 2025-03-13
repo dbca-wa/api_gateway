29df2f91c6c85257222660bcdc7c53f96e76292f
Katsufumi Shibata <shibaken@gmail.com>
2025-03-13 11:45:14 +0800

    Import json

--- apigw/settings.py
+++ apigw/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from confy import env
 import os
 import decouple
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
