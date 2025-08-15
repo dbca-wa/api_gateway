819f311f4db957b0f37a1703513ea60dbcbd9410
Jason Moore <jmoore@austwa.com>
2025-08-15 12:41:15 +0800

    Update utils.py

--- apigw/utils.py
+++ apigw/utils.py
@@ -4,6 +4,7 @@ def clean_url(value):
     value = value.replace("'", "%27")
     value = value.replace("&", "%26")
     value = value.replace("/", "%2F")
+    value = value.replace(",", "%2C")    
     return value
 
 
